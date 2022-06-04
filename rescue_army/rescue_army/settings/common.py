286769bd24e9892def1ffd392ccc1700143807e0
Meghanath Nalawade <50794140+mr-compilerMS@users.noreply.github.com>
2022-06-04 13:13:09 +0530

    heroku deploy attempt 3

--- rescue_army/rescue_army/settings/common.py
+++ rescue_army/rescue_army/settings/common.py
@@ -7,7 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 import os
 
 
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -111,6 +110,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATIC_URL = "static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
